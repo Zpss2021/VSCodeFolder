acca0d11d27c9f6357e37ccf7b017c8bac551fbd
Zpss <2125060196@henu.edu.cn>
2022-06-16 01:30:24 +0800

    尝试在 VSCode 中的 msys2 中用 zsh 终端向 git 中提交内容

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+    int a;
+    printf("Please input a:");
+    scanf("%d", &a);
+    printf("a = %d\n", a);
+    system("pause");
+    return 0;
+}
