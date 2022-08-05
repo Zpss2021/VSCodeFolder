01391f63068de16554a703da117f755a439e41b7
Zpss <2125060196@henu.edu.cn>
2022-08-05 10:24:09 +0800

    Add menu in main.c

--- main.c
+++ main.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void menu() {
+	printf("--------HELLO--WORLD--PROGRAM--------\n");
+	printf("--------HELLO--WORLD--PROGRAM--------\n");
+	printf("--------HELLO--WORLD--PROGRAM--------\n");
+	printf("--------HELLO--WORLD--PROGRAM--------\n");
+}
+
 int main(void) {
     int a;
+	menu();
     printf("Please input a:");
     scanf("%d", &a);
     printf("a = %d\n", a);
